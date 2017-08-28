14dd69f852c947bd4eb3330052ed3ceece837963
ketchupyan <hugyanyi@gmail.com>
2017-08-28 21:38:45 +0800

    Add files via upload

---
+++ request.js
@@ -0,0 +1,35 @@
+// const heeps = require('https')
+
+// https.get('http://api.douban.com/v2/movie/top250?count=5',
+//     resp => {
+//     let data = ''
+//     resp.on('data', chunk => {
+//         data += chunk;
+//     })
+//     resp.on('end', () =>{
+//         console.log(JSON.parse(data))
+//     })
+// }).on("error", err => {
+//     console.log('Error:' + err.message)
+// })
+
+// const request = require('request')
+
+// request('http://api.douban.com/v2/movie/top250?count=5',
+//  {
+//     json:true
+// }, (err, res, body) =>{
+//     if(err) 
+//         return console.log(err)
+//     console.log(body)
+// })
+
+const axios = require ('axios')
+axios
+    .get('http://api.douban.com/v2/movie/top250?count=5')
+    ,then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.log(error)
+    })
